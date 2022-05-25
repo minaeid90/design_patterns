509c040114d93c36ac8524f47ab9c9d7950c84e8
Mina <minaeid90@gmail.com>
2022-05-25 23:44:12 +0200

    finish prototype factory design pattern
    Signed-off-by: Mina <minaeid90@gmail.com>

--- 4_prototype/2_prototype_factory.py
+++ 4_prototype/2_prototype_factory.py
@@ -1,5 +1,6 @@
 import copy
 
+
 class Address:
     def __init__(self, street_address, suite, city):
         self.street_address = street_address
@@ -8,6 +9,7 @@ class Address:
 
     def __str__(self):
         return f'{self.street_address}, Suite #{self.suite}, {self.city}'
+
 
 class Employee:
     def __init__(self, name, address):
@@ -18,4 +20,26 @@ class Employee:
         return f'{self.name} works at {self.address}'
 
 
-        
+class EmployeeFactory:
+    main_office_employee = Employee('', Address("123 East Dr", 0, "London"))
+    aux_office_employee = Employee('', Address('123B East Dr', 0, "London"))
+
+    @staticmethod
+    def __new_employee(proto, name, suite):
+        result = copy.deepcopy(proto)
+        result.name = name
+        result.address.suite = suite
+        return result
+
+    @staticmethod
+    def new_main_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(EmployeeFactory.main_office_employee, name, suite)
+
+    @staticmethod
+    def new_aux_office_employee(name, suite):
+        return EmployeeFactory.__new_employee(EmployeeFactory.aux_office_employee, name, suite)
+
+john = EmployeeFactory.new_main_office_employee('John',1)
+jane = EmployeeFactory.new_aux_office_employee('Jane', 2)
+print(john)
+print(jane)
