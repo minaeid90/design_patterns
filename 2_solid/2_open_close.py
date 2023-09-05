20922bb9013ad301b0967bfe36baa5969d24430b
Mina Eid <minaeid90@Minas-MacBook-Air.local>
2023-09-05 20:17:59 +0300

    adater, not completed

--- 2_solid/2_open_close.py
+++ 2_solid/2_open_close.py
@@ -85,7 +85,7 @@ class AndSpecification(Specification):
 class BetterFilter(Filter):
     def filter(self, items, spec):
         for item in items:
-            if spec.is_satisfied(item):
+            if spec.is_satisfied(item): 
                 yield item
 
 
---
+++ 7_adapter/1_adapter.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def draw_point(p):
+    print(".", end='')
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+# not completed
